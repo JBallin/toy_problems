ffef64641179a468311a44fd55d108a7ae5d7bdd
JBallin <JBallin@users.noreply.github.com>
2018-06-06 12:34:11 -0700

    Add add_newline_to_prompt.sh

---
+++ other/self_made/add_newline_to_prompt.sh
@@ -0,0 +1,22 @@
+### Write script that adds a new line between every command
+
+# Constraints:
+#   1) The first line should not have a new line (it should start on top)
+#   2) The script should not interfere with PROMPT_COMMAND's expected behavior
+
+add_newline_to_prompt() {
+  is_new_login="true"
+  INIT_PROMPT_COMMAND="$PROMPT_COMMAND"
+  DEFAULT_PROMPT_COMMAND=update_terminal_cwd
+  PROMPT_COMMAND='{
+    if [ $is_new_login = "true" ]; then
+      is_new_login="false"
+      eval $INIT_PROMPT_COMMAND
+    else
+      PS1="\n$PS1"
+      PROMPT_COMMAND=$DEFAULT_PROMPT_COMMAND
+    fi
+  }'
+}
+
+add_newline_to_prompt
