6198341fa50b88df71a46fdde1f81201af5253f6
JBallin <JBallin@users.noreply.github.com>
2018-06-09 23:18:53 -0700

    Add load_upgrade_nvm.sh

---
+++ other/self_made/shell_files/load_upgrade_nvm.sh
@@ -0,0 +1,7 @@
+# write bash scripts that load nvm (incuding bash bash_completion) and upgrade_nvm
+# both should be done manually so as to not mess with path, reduce shell startup_time, and allow usage of system nvm by default
+
+NVM_DIR="$HOME/.nvm"
+load_nvm_bash_completion='[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion'
+alias nvm_load='[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh && eval $load_nvm_bash_completion'
+alias nvm_upgrade='(cd $NVM_DIR && git fetch --tags origin && git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` && . $NVM_DIR/nvm.sh)'
