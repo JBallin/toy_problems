5eb13b4975bcec8009867194e15151540accb25f
JBallin <JBallin@users.noreply.github.com>
2018-06-13 11:24:55 -0700

    Add gist_update_apm and refactor gist_updates.sh

--- other/self_made/shell_files/gist_updates.sh
+++ other/self_made/shell_files/gist_updates.sh
@@ -4,20 +4,55 @@
 gist_bp_id=ID_HERE
 gist_cask_id=ID_HERE
 gist_brew_id=ID_HERE
-
+gist_apm_id=ID_HERE
 update_gist() {
   gist -u $1 $2
 }
-alias gist_bp="update_gist $gist_bp_id ~/.bash_profile"
-gist_brew() {
-  brew list > brew_list
-  brew leaves > brew_leaves
-  update_gist $gist_brew_id 'brew_list brew_leaves'
-  rm brew_list brew_leaves
+gist_is_changed() {
+  # takes local_file_name and gist_id
+  gist -r $2 $1 > gist_raw
+  local changed
+  if cmp --quiet $1 gist_raw; then
+    echo "$1 already up to date!"
+    changed=1
+  else
+    echo "Updating $1..."
+    changed=0
+  fi
+  rm gist_raw
+  return $changed
 }
-gist_cask() {
+gist_update_bp() {
+  # parens make code run in separate shell so that cwd doesn't change
+  (cd ~
+  if gist_is_changed .bash_profile $gist_bp_id; then
+    update_gist $gist_bp_id .bash_profile
+  fi)
+}
+gist_update_brew() {
+  brew list > brew_list
+  if gist_is_changed brew_list $gist_brew_id; then
+    update_gist $gist_brew_id brew_list
+  fi
+  rm brew_list
+  brew leaves > brew_leaves
+  if gist_is_changed brew_leaves $gist_brew_id; then
+    update_gist $gist_brew_id brew_leaves
+  fi
+  rm brew_leaves
+}
+gist_update_cask() {
   brew cask list > brew_cask
-  update_gist $gist_cask_id brew_cask
+  if gist_is_changed brew_cask $gist_cask_id; then
+    update_gist $gist_cask_id brew_cask
+  fi
   rm brew_cask
 }
-alias gist_all='gist_bp && gist_brew && gist_cask'
+gist_update_apm() {
+  apm list | sed -n -e '/Community Packages/,$p' > apm_list
+  if gist_is_changed apm_list $gist_apm_id; then
+    update_gist $gist_apm_id apm_list
+  fi
+  rm apm_list
+}
+alias gist_update_all='gist_update_bp && gist_update_brew && gist_update_cask && gist_update_apm'
