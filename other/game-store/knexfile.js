c4cef6e720e54a878aa8698cc5f89d95f69520ae
JBallin <JBallin@users.noreply.github.com>
2018-06-19 11:17:32 -0700

    Add knex files

---
+++ other/game-store/knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/gamestore_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/gamestore_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
