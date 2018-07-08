ebc4d839fd15b3a9f40ba52b962f0f0ae75cfe0c
JBallin <JBallin@users.noreply.github.com>
2018-07-08 15:31:18 -0700

    Cleanup

--- other/self_made/birds-app/app.js
+++ other/self_made/birds-app/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
-const birds = require('/routes/birds');
+const birds = require('./routes/birds');
+
+app.use(express.static(__dirname + '/public'))
 
 app.use('/birds', birds);
+
+app.listen(3000);
+
+module.exports = app;
--- other/self_made/birds-app/routes/birds.js
+++ other/self_made/birds-app/routes/birds.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex')
+const path = require('path');
 
-router.use('/', () => console.log('run every time hit /birds'));
-router.get('/', () => res.sendFile('/public/birds.html'));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public/birds.html')));
+
 router.get('/about', () => {
   knex('birds')
   .then()
 })
+
 router.post('/')
+
 router.put('/')
+
 router.delete('/')
+
+
+module.exports = router;
