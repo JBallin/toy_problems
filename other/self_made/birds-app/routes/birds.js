f823dacfead3009289945af789bc0cab60739675
JBallin <JBallin@users.noreply.github.com>
2018-07-08 14:16:32 -0700

    Init birds route

---
+++ other/self_made/birds-app/routes/birds.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex')
+
+router.use('/', () => console.log('run every time hit /birds'));
+router.get('/', () => res.sendFile('/public/birds.html'));
+router.get('/about', () => {
+  knex('birds')
+  .then()
+})
+router.post('/',
