674b2a8f4f8ceb7d1658f98aaa00a190bdff2475
JBallin <JBallin@users.noreply.github.com>
2018-07-08 14:16:52 -0700

    Add birds migration

---
+++ other/self_made/birds-app/migrations/20180708135339_birds.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('birds', table => {
+    table.increments()
+    table.string('type').notNullable()
+    table.text('description')
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = (knex, Promise) => knex.schema.dropTable('birds');
