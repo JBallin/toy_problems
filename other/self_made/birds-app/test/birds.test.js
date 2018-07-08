e061468f79ca1922a811ee7666565d7b0bbf2d49
JBallin <JBallin@users.noreply.github.com>
2018-07-08 15:30:57 -0700

    Init /birds test

---
+++ other/self_made/birds-app/test/birds.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /birds', () => {
+  it('respond with status 200', done => {
+    request(app)
+      .get('/birds')
+      .expect(200, done);
+  })
+})
