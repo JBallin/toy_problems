c018d1eb5a9d975cb818d0c9e59afc77db5985ad
JBallin <JBallin@users.noreply.github.com>
2018-07-09 17:36:47 -0700

    Add created_at and update_at to seeds

--- other/self_made/birds-app/seeds/001_birds.js
+++ other/self_made/birds-app/seeds/001_birds.js
@@ -2,9 +2,24 @@ exports.seed = function(knex, Promise) {
   return knex('birds').del()
     .then(function () {
       return knex('birds').insert([
-        {type: 'Bluejay', description: 'Blue and stuff'},
-        {type: 'Sparrow', description: 'Likes to eat worms'},
-        {type: 'Seagull', description: 'Likes to eat fish'}
+        {
+          type: 'Bluejay',
+          description: 'Blue and stuff',
+          created_at: '2018-07-09T22:36:23.975Z',
+          updated_at: '2018-07-09T22:36:23.975Z'
+        },
+        {
+          type: 'Sparrow',
+          description: 'Likes to eat worms',
+          created_at: '2018-07-09T22:36:23.975Z',
+          updated_at: '2018-07-09T22:36:23.975Z'
+        },
+        {
+          type: 'Seagull',
+          description: 'Likes to eat fish',
+          created_at: '2018-07-09T22:36:23.975Z',
+          updated_at: '2018-07-09T22:36:23.975Z'
+       }
       ]);
     });
 };
