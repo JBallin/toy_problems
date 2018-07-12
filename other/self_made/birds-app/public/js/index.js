4cff98cd5e9456229f815a6a3ade7350d6d092c9
JBallin <JBallin@users.noreply.github.com>
2018-07-12 11:39:41 -0700

    Display birds types and info on home page

--- other/self_made/birds-app/public/js/index.js
+++ other/self_made/birds-app/public/js/index.js
@@ -2,7 +2,13 @@ const bird1 = document.querySelector('#bird1');
 const bird2 = document.querySelector('#bird2');
 const bird3 = document.querySelector('#bird3');
 
-// fetch('birds')
-// .then(res => {
-//   console.log(res.body);
-// })
+fetch('birds')
+.then(res => {
+  return res.json();
+})
+.then(birds => {
+  const displayBird = n => birds[n-1].type + ': ' + birds[n-1].description;
+  bird1.innerText = displayBird(1);
+  bird2.innerText = displayBird(2);
+  bird3.innerText = displayBird(3);
+})
