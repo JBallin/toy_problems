2de0a7fadcb708a243183609c332de3ae265781c
JBallin <JBallin@users.noreply.github.com>
2018-07-12 18:48:58 -0700

    Write remove route

--- other/self_made/birds-app/routes/remove.js
+++ other/self_made/birds-app/routes/remove.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../knex')
+
+router.delete('/:id', (req, res) => {
+  knex('birds')
+  .where('id', req.params.id)
+  .del()
+  .returning(['id', 'type', 'description'])
+  .then(removed => {
+    console.log('removed:', removed[0]);
+    res.redirect('/');
+  })
+})
+
+module.exports = router;
