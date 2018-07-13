5501593c6f75f044a382a6b53e8312a903b94598
JBallin <JBallin@users.noreply.github.com>
2018-07-12 18:40:24 -0700

    Init remove route

--- other/self_made/birds-app/app.js
+++ other/self_made/birds-app/app.js
@@ -6,6 +6,7 @@ const birds = require('./routes/birds');
 const add = require('./routes/add');
 const about = require('./routes/about');
 const edit = require('./routes/edit');
+const remove = require('./routes/remove');
 
 // use routes
 app.use(express.static('public'))
@@ -13,6 +14,7 @@ app.use('/birds', birds);
 app.use('/add', add);
 app.use('/about', about);
 app.use('/edit', edit);
+app.use('/remove', remove)
 
 // start server if not test
 if (process.env.NODE_ENV !== 'test') {
