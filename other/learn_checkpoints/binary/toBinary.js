f9267390ac503decc308beb8017cb0ebf97d5ebe
JBallin <JBallin@users.noreply.github.com>
2018-08-22 09:55:22 -0700

    Write toBinary function

---
+++ other/learn_checkpoints/binary/toBinary.js
@@ -0,0 +1,11 @@
+function toBin(n) {
+  let bin = '';
+  let remain = n;
+  while (remain > 0) {
+    bin = (remain % 2).toString() + bin;
+    remain = Math.floor(remain / 2);
+  }
+  return bin;
+}
+
+module.exports = toBin;
