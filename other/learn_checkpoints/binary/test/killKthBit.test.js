66704962f19061535960f65a1aab1b9f59571652
JBallin <JBallin@users.noreply.github.com>
2018-08-22 11:23:35 -0700

    Write killKthBit tests

---
+++ other/learn_checkpoints/binary/test/killKthBit.test.js
@@ -0,0 +1,14 @@
+const { assert } = require('chai');
+const killKthBit = require('../killKthBit');
+
+describe('killKthBit', () => {
+  it('should return a number', () => {
+    assert.isNumber(killKthBit(10, 2));
+  });
+  it('should "kill" kth bit if its 1', () => {
+    assert.equal(killKthBit(10, 2), 8);
+  });
+  it('should return same decimal if kth bit is 0', () => {
+    assert.equal(killKthBit(8, 2), 8);
+  });
+});
