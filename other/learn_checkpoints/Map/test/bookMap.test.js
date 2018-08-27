3c55327201518e943fddc5c06e2fa07ac61b6a7a
JBallin <JBallin@users.noreply.github.com>
2018-08-27 16:26:21 -0700

    Write BookMap tests

---
+++ other/learn_checkpoints/Map/test/bookMap.test.js
@@ -0,0 +1,20 @@
+const { assert } = require('chai');
+const { BookMap } = require('../BookMap');
+
+describe('BookMap', () => {
+  const bookMap = new BookMap();
+  const regMap = new Map();
+  it('should add a book as an array', () => {
+    assert.deepEqual(bookMap.add('jballin', 'git').library, regMap.set('jballin', ['git']));
+  });
+  it('should add to authors books array if already has a book', () => {
+    assert.deepEqual(bookMap.add('jballin', 'bash').library, regMap.set('jballin', ['git', 'bash']));
+  });
+  it('should retreive books written by author', () => {
+    assert.deepEqual(bookMap.retrieve('jballin'), ['git', 'bash']);
+  });
+  it('should return size', () => {
+    bookMap.add('bossman', 'this is how we do it');
+    assert.deepEqual(bookMap.size(), 2);
+  });
+});
