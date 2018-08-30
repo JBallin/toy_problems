ea74339a1fc82c95f0aa9d429472e977b652a2a2
JBallin <JBallin@users.noreply.github.com>
2018-08-29 17:13:17 -0700

    Solve linearSearch iterative

--- other/learn_checkpoints/searching_sorting/js/linearSearch.js
+++ other/learn_checkpoints/searching_sorting/js/linearSearch.js
@@ -5,3 +5,10 @@ const linearSearchI = (arr, n) => {
   return -1;
 };
 
+const linearSearchR = (arr, n, i=0) => {
+  if (i === arr.length) return -1
+  if (arr[i] === n) return i;
+  return linearSearchR(arr, n, i + 1);
+};
+
+module.exports = { linearSearchI, linearSearchR };
