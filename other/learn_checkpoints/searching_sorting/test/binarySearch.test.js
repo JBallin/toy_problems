161d7bbf188d9a1ff86559730d79126e116b22a5
JBallin <JBallin@users.noreply.github.com>
2018-08-29 21:35:27 -0700

    Refactor binarySearch test

--- other/learn_checkpoints/searching_sorting/test/binarySearch.test.js
+++ other/learn_checkpoints/searching_sorting/test/binarySearch.test.js
@@ -2,45 +2,35 @@ const { assert } = require('chai');
 const { binarySearchI, binarySearchR } = require('../js/binarySearch');
 
 describe('binary search', () => {
-  const inputArr = [1, 5, 8, 12, 15];
-  const target = 12;
-  const expected = 3;
+  const arr = [1, 5, 8, 12, 15];
 
   describe('iterative', () => {
-    const validActual = binarySearchI(inputArr, target);
-    const invalidActual = binarySearchI(inputArr, 100);
-    const firstActual = binarySearchI(inputArr, 1);
-    const lastActual = binarySearchI(inputArr, 15);
     it('should return index of target', () => {
-      assert.equal(validActual, expected);
+      assert.equal(binarySearchI(arr, 12), 3);
     });
     it('should return -1 if target not in array', () => {
-      assert.equal(invalidActual, -1);
+      assert.equal(binarySearchI(arr, 100), -1);
     });
     it('should return index of first number in arr', () => {
-      assert.equal(firstActual, 0);
+      assert.equal(binarySearchI(arr, 1), 0);
     });
     it('should return index of last number in arr', () => {
-      assert.equal(lastActual, 4);
+      assert.equal(binarySearchI(arr, 15), 4);
     });
   });
 
   describe('recursive', () => {
-    const validActual = binarySearchR(inputArr, target);
-    const invalidActual = binarySearchR(inputArr, 100);
-    const firstActual = binarySearchR(inputArr, 1);
-    const lastActual = binarySearchR(inputArr, 15);
     it('should return index of target', () => {
-      assert.equal(validActual, expected);
+      assert.equal(binarySearchR(arr, 12), 3);
     });
     it('should return -1 if target not in array', () => {
-      assert.equal(invalidActual, -1);
+      assert.equal(binarySearchR(arr, 100), -1);
     });
     it('should return index of first number in arr', () => {
-      assert.equal(firstActual, 0);
+      assert.equal(binarySearchR(arr, 1), 0);
     });
     it('should return index of last number in arr', () => {
-      assert.equal(lastActual, 4);
+      assert.equal(binarySearchR(arr, 15), 4);
     });
   });
 });
