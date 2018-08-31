53052913676af73c70988e08bcc39cab9ab3d196
JBallin <JBallin@users.noreply.github.com>
2018-08-30 22:15:43 -0700

    Write getMinI helper func

--- other/learn_checkpoints/searching_sorting/js/sort-helpers.js
+++ other/learn_checkpoints/searching_sorting/js/sort-helpers.js
@@ -4,4 +4,12 @@ const swap = (a, i, j) => {
   return r;
 };
 
-module.exports = { swap };
+const getMinI = (arr, start) => {
+  let minI = start;
+  for (let i = start + 1; i < arr.length; i += 1) {
+    if (arr[i] < arr[minI]) minI = i;
+  }
+  return minI;
+}
+
+module.exports = { swap, getMinI };
