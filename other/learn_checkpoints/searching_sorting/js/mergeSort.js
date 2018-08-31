271a4c211fd65d3187972a8da47ecbdbf0ed0e61
JBallin <JBallin@users.noreply.github.com>
2018-08-31 14:09:43 -0700

    Refactor

--- other/learn_checkpoints/searching_sorting/js/mergeSort.js
+++ other/learn_checkpoints/searching_sorting/js/mergeSort.js
@@ -1,22 +1,16 @@
 const { mergeI, mergeR } = require('./merge');
 
 function mergeSortI(arr) {
-  if (!arr.length) return [];
-  let res = [];
-  for (let i = 0; i < arr.length; i += 1) res.push([arr[i]]);
+  let res = arr.map(e => [e]);
 
   while (res.length > 1) {
-    let temp = [];
-    for (let i = 0; i < res.length - 1; i += 2) {
-      temp.push(mergeI(res[i], res[i + 1]));
-    }
-    if (res.length % 2 !== 0) {
-      temp.push(res[res.length - 1]);
-    }
-    [res, temp] = [temp, []];
+    const temp = [];
+    for (let i = 0; i < res.length - 1; i += 2) temp.push(mergeI(res[i], res[i + 1]));
+    if (res.length % 2 !== 0) temp.push(res[res.length - 1]);
+    res = temp;
   }
 
-  return res[0];
+  return res[0] || [];
 }
 
 const mergeSortR = (arr) => {
