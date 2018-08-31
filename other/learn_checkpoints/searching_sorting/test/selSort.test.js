4f9fc500555b22325c6b7dbb10d6f41d6974ef15
JBallin <JBallin@users.noreply.github.com>
2018-08-30 21:44:15 -0700

    Write selectionSort tests

---
+++ other/learn_checkpoints/searching_sorting/test/selSort.test.js
@@ -0,0 +1,46 @@
+const { assert } = require('chai');
+const { selSortI, selSortR } = require('../js/selSort');
+
+describe('merge sort', () => {
+  let a;
+  const original = [4, 2, 9, 1, 7];
+  const sorted = [1, 2, 4, 7, 9];
+
+  beforeEach(() => {
+    a = original.slice();
+  });
+
+  describe('iterative', () => {
+    const result = selSortI(a);
+    it('should return an Array', () => {
+      assert.isArray(result);
+    });
+    it('should return same length as original', () => {
+      assert.lengthOf(result, original.length);
+    });
+    it('should return a new sorted array', () => {
+      assert.deepEqual(result, sorted);
+      assert.notEqual(result, original);
+    });
+    it('should return an empty array if input array is empty', () => {
+      assert.deepEqual(selSortI([]), []);
+    });
+  });
+
+  describe('recursive', () => {
+    const result = selSortR(a);
+    it('should return an Array', () => {
+      assert.isArray(result);
+    });
+    it('should return same length as original', () => {
+      assert.lengthOf(result, original.length);
+    });
+    it('should return a new sorted array', () => {
+      assert.deepEqual(result, sorted);
+      assert.notEqual(result, original);
+    });
+    it('should return an empty array if input array is empty', () => {
+      assert.deepEqual(selSortR([]), []);
+    });
+  });
+});
