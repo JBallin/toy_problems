d200ff7a18f5b9b46b2a5ce1192c518ceb90136d
JBallin <JBallin@users.noreply.github.com>
2018-08-31 13:26:57 -0700

    Convert 'expected' Object to Map

--- other/learn_checkpoints/custom_data_structures/test/groupBy.test.js
+++ other/learn_checkpoints/custom_data_structures/test/groupBy.test.js
@@ -6,25 +6,26 @@ describe('groupBy', () => {
     { color: 'green', size: 'medium' },
     { color: 'red', size: 'large' },
     { color: 'orange', size: 'medium' },
-    { color: 'green', size: 'small' }
+    { color: 'green', size: 'small' },
   ];
-  const expected = {
+  const expectedObj = {
     green: [
       { color: 'green', size: 'medium' },
-      { color: 'green', size: 'small' }
+      { color: 'green', size: 'small' },
     ],
     red: [
-      { color: 'red', size: 'large' }
+      { color: 'red', size: 'large' },
     ],
     orange: [
-      { color: 'orange', size: 'medium' }
-    ]
+      { color: 'orange', size: 'medium' },
+    ],
   };
+  const expectedMap = new Map(Object.entries(expectedObj));
 
   it('should use Map', () => {
     assert.include(groupBy.toString(), 'Map');
   });
   it('should group items', () => {
-    assert.deepEqual(groupBy(items, 'color'), expected);
+    assert.deepEqual(groupBy(items, 'color'), expectedMap);
   });
 });
