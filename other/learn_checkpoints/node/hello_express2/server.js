81b04bcd388984c56f163bc8de0f0527f7c3793d
JBallin <JBallin@users.noreply.github.com>
2018-10-14 14:35:56 -0700

    Say hello to req.params.name

--- other/learn_checkpoints/node/hello_express2/server.js
+++ other/learn_checkpoints/node/hello_express2/server.js
@@ -6,3 +6,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('listening on port', port));
+
+app.get('/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello ${name}!`);
+});
