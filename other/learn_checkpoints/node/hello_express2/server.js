50977186efcbc482639544b06e74557919e35b2b
JBallin <JBallin@users.noreply.github.com>
2018-10-14 15:14:44 -0700

    Add server-side rendered EJS home page

--- other/learn_checkpoints/node/hello_express2/server.js
+++ other/learn_checkpoints/node/hello_express2/server.js
@@ -5,7 +5,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.listen(port, () => console.log('listening on port', port));
+
+app.get('/', (req, res) => {
+  res.render('index', { header: 'Best Site Ever', text: 'Welcome!' });
+});
 
 app.get('/:name', (req, res) => {
   const { name } = req.params;
