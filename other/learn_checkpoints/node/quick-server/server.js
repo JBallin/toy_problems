d17b61fb30ca14a68cea4ccbb0920cf47bb28aff
JBallin <JBallin@users.noreply.github.com>
2018-10-15 16:41:08 -0700

    Respond with simple JSON

--- other/learn_checkpoints/node/quick-server/server.js
+++ other/learn_checkpoints/node/quick-server/server.js
@@ -8,4 +8,6 @@ app.use(logger('dev'));
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => res.json({ data: [1, 2, 3] }));
+
 app.listen(port, () => console.log(`listening on port ${port}`)); // eslint-disable-line no-console
