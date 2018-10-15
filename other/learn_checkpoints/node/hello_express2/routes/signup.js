73cca8d570fe351183b5a35a72092a21fc7d0307
JBallin <JBallin@users.noreply.github.com>
2018-10-15 11:44:55 -0700

    Throw error if POST /signup missing required form fields

--- other/learn_checkpoints/node/hello_express2/routes/signup.js
+++ other/learn_checkpoints/node/hello_express2/routes/signup.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => res.sendFile(signupPath));
 
 router.post('/', express.json(), express.urlencoded({ extended: true }), (req, res) => {
   const { first_name: first, last_name: last } = req.body;
+  if (!first || !last) {
+    res.status(400);
+    throw new Error('Signup form data required');
+  }
   res.render('index', { header: 'Sign Up Complete', text: `Thanks for joining ${`${first} ${last}`}!` });
 });
 
