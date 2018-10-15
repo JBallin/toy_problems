87d79d1119041255c21cce46c0ebd7c686ac3be8
JBallin <JBallin@users.noreply.github.com>
2018-10-15 12:21:01 -0700

    Write routes

--- other/learn_checkpoints/node/request-the-rainbow-get-post-routes/app.js
+++ other/learn_checkpoints/node/request-the-rainbow-get-post-routes/app.js
@@ -7,7 +7,14 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-// Write your routes here!
+app.get('/red', (req, res) => res.send({ color: 'red' }));
+app.get('/yellow', (req, res) => res.send({ color: 'yellow' }));
+app.get('/blue', (req, res) => res.send({ color: 'blue' }));
+app.get('/violet', (req, res) => res.send({ color: 'violet' }));
+app.post('/orange', (req, res) => res.send({ color: 'orange' }));
+app.post('/green', (req, res) => res.send({ color: 'green' }));
+app.post('/indigo', (req, res) => res.send({ color: 'indigo' }));
+app.post('/red', (req, res) => res.send({ color: 'red' }));
 
 app.use((err, req, res) => {
   const status = err.status || 500;
