d1ebfd3cf31415c6e843b51802aca604006cb972
JBallin <JBallin@users.noreply.github.com>
2018-10-15 16:40:17 -0700

    Add logging through morgan

--- other/learn_checkpoints/node/quick-server/server.js
+++ other/learn_checkpoints/node/quick-server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require('morgan');
 
 const app = express();
 
 app.disable('x-powered-by');
+app.use(logger('dev'));
 
 const port = process.env.PORT || 3000;
 
