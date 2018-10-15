3a8f639a545f2d7343af2486cf8f7fabc17a012d
JBallin <JBallin@users.noreply.github.com>
2018-10-15 16:38:34 -0700

    Disable x powered by

--- other/learn_checkpoints/node/quick-server/server.js
+++ other/learn_checkpoints/node/quick-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.disable('x-powered-by');
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`)); // eslint-disable-line no-console
